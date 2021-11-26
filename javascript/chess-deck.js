cd7fe05a9eaf684d310bbd8bec3564c1f9724f1a
chix_006 <s24b_stepanov@179.ru>
2021-11-26 04:10:07 +0300

    all tasks completed

--- javascript/chess-deck.js
+++ javascript/chess-deck.js
@@ -1,17 +1,18 @@
-start_position = {'pawn_1': ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'],
-         'bishop_1': ['c1', 'f1'],
-         'knight_1': ['b1', 'g1'],
-         'rook_1': ['a1', 'h1'],
-         'queen_1': ['d1'],
-         'king_1': ['e1'],
-         'pawn_2': ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'],
-         'bishop_2': ['c8', 'f8'],
-         'knight_2': ['b8', 'g8'],
-         'rook_2': ['a8', 'h8'],
-         'queen_2': ['d8'],
-         'king_2':['e8']
+start_position = {'pawn_1': [['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'], '2.png'],
+         'bishop_1': [['c1', 'f1'], '6.png'],
+         'knight_1': [['b1', 'g1'], '4.png'],
+         'rook_1': [['a1', 'h1'], '8.png'],
+         'queen_1': [['d1'], '10.png'],
+         'king_1': [['e1'], '12.png'],
+         'pawn_2': [['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'], '14.png'],
+         'bishop_2': [['c8', 'f8'], '18.png'],
+         'knight_2': [['b8', 'g8'], '16.png'],
+         'rook_2': [['a8', 'h8'], '20.png'],
+         'queen_2': [['d8'], '22.png'],
+         'king_2': [['e8'], '24.png']
 }
-postion = start_position
+let postion = start_position
+let current_cell = null
 function generate_board(side_px) {
     let letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     let table = document.createElement("table");
@@ -32,8 +33,7 @@ function generate_board(side_px) {
             }
             td.style.width = (side_px / 8).toString() + 'px';
             td.style.height = (side_px / 8).toString() + 'px';
-            td.onclick = replace
-            console.log(td.style.width, td.style.height, td.id)
+            td.onclick = click_table
             tr.appendChild(td);
         }
         table.appendChild(tr);
@@ -43,7 +43,7 @@ function generate_board(side_px) {
     return table
 }
 
-function set_img(cell, path_to_img = 'img.png') {
+function set_img(cell, path_to_img) {
     let image = document.createElement('img');
     image.src = path_to_img;
     image.style = "max-height:100%; max-width:100%";
@@ -54,30 +54,38 @@ function set_img(cell, path_to_img = 'img.png') {
 }
 
 function delete_img(cell) {
-    document.getElementById(cell).removeChild(document.getElementById(cell).firstChild);
-    console.log(document.body);
+    if (document.getElementById(cell).firstChild != null){
+        document.getElementById(cell).removeChild(document.getElementById(cell).firstChild);
+    }
+
 }
-function replace(){
-    return 0
+function click_table(){
+    if (current_cell == null){
+        current_cell = [this.id, document.getElementById(this.id).firstChild.attributes[0].nodeValue]
+    }else{
+        console.log(current_cell[1])
+        delete_img(this.id);
+        set_img(this.id, current_cell[1]);
+        delete_img(current_cell[0]);
+        current_cell = null;
+    }
+
 }
 
 function start_game() {
-    console.log('start')
-    let im = 'img.png'
+    console.log('start');
     for (let i in start_position){
-        console.log(start_position[i])
-        for (let j in start_position[i]){
-            console.log(start_position[i][j])
-            set_img(start_position[i][j])
+        for (let j in start_position[i][0]){
+            set_img(start_position[i][0][j], start_position[i][1]);
         }
     }
 }
 function restart_game(){
-    for (let i in start_position){
-        console.log(start_position[i])
-        for (let j in start_position[i]){
-            console.log(start_position[i][j])
-            delete_img(start_position[i][j])
+    let lett = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    for (let i in lett){
+        for (let j = 0; j < 8; j++){
+            console.log(lett[i]+j.toString())
+            delete_img(lett[i]+(j+1).toString())
         }
     }
     start_game()
